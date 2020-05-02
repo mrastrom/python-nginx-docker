ab9b5deac8b46595dddf8282f56d1d99cf6cb6eb
Mats <mats.astrom@arbetsformedlingen.se>
2020-05-02 18:36:23 +0200

    Change to templates

--- app/app.py
+++ app/app.py
@@ -1,10 +1,12 @@
 # app.py - a minimal flask api using flask_restful
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask_restful import Resource, Api, reqparse
 
-app = Flask(__name__)
+app = Flask( __name__, template_folder="templates" )
 api = Api(app)
 
+
+# Method one to add routes
 class HelloWorld(Resource):
     @staticmethod
     def get():
@@ -12,7 +14,14 @@ class HelloWorld(Resource):
         return {'hello': name}
 
 
-api.add_resource(HelloWorld, '/hello')
+api.add_resource(HelloWorld, '/app/hello')
+
+
+# Method teo to add routes
+@app.route("/")
+def home_page():
+    return render_template("index.html")
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug='true')
